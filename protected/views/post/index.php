eb36fded58ba26ec2a17dfd594f0c79c2c648c58
root <root@oxfard.fvds.ru>
2020-01-30 10:14:20 +0000

    Add project

--- protected/views/post/index.php
+++ protected/views/post/index.php
@@ -1,4 +1,10 @@
-
+<?php if(Yii::app()->user->hasFlash('signUp')) { ?>
+ 
+    <div class="flash-success">
+        <?php echo Yii::app()->user->getFlash('signUp'); ?>
+    </div>
+ 
+<?php } ?>
 
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
